ffb522942434a3063daadc250b0cc1631ce191d9
spoony-z <shenjun_y@yeah.net>
2023-05-08 14:38:42 +0000

    删除文件 vuepressV2/.vuepress/dist

--- vuepressV2/.vuepress/dist/assets/InstallIntroduc.html-f7a18252.js
+++
@@ -1,4 +0,0 @@
-const data = JSON.parse('{"key":"v-2a84decf","path":"/blogs/InstallIntroduc.html","title":"安装引入","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"npm 安装","slug":"npm-安装","link":"#npm-安装","children":[]},{"level":2,"title":"全部引入","slug":"全部引入","link":"#全部引入","children":[]},{"level":2,"title":"按需引入","slug":"按需引入","link":"#按需引入","children":[]}],"git":{"createdTime":1683465114000,"updatedTime":1683465114000,"contributors":[{"name":"spoony_Z","email":"shenjun_y@yeah.net","commits":1}]},"filePathRelative":"blogs/InstallIntroduc.md"}');
-export {
-  data
-};
--- vuepressV2/.vuepress/dist/assets/index.html-fc1f0a17.js
+++
@@ -1,4 +0,0 @@
-const data = JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"","lang":"en-US","frontmatter":{"home":true,"modules":["BannerBrand","MdContent","Footer"],"bannerBrand":{"bgImage":"/bg.svg","title":"168wangxiao-ui","description":"一个基于 vue2 和 Element-ui 二次封装的组件库。","tagline":"vuepress-theme-reco 2.0 继续坚持简洁的风格，所有功能开箱即用，首页模块化组装，使用 tailwindcss 书写样式，将 Vite 作为默认编译器。你只需要负责内容创作，其他请交给我。","buttons":[{"text":"开 始","link":"/blogs/InstallIntroduc"},{"text":"了解作者","link":"/docs/style-default-api/introduce","type":"plain"}],"socialLinks":[{"icon":"LogoGithub","link":"https://github.com/vuepress-reco/vuepress-theme-reco"},{"icon":"LogoGithub","link":"https://github.com/vuepress-reco/vuepress-theme-reco"}]}},"headers":[],"git":{"createdTime":1683341156000,"updatedTime":1683465186000,"contributors":[{"name":"spoony_Z","email":"shenjun_y@yeah.net","commits":3}]},"filePathRelative":"README.md"}');
-export {
-  data
-};
