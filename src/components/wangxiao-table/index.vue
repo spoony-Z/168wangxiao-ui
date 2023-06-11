f416e6ab06177de5c1baef9ba32c6ff61a979410
杨胜军 <shenjun_y@yeah.net>
2023-06-11 14:11:22 +0800

    提交

--- src/components/wangxiao-table/index.vue
+++ src/components/wangxiao-table/index.vue
@@ -6,17 +6,31 @@
       </div>
       <div>
         <slot name="table-right-btn"></slot>
-        <el-button icon="el-icon-setting" size="small" @click="dialogVisible = true">自定义列</el-button>
+        <el-button
+          icon="el-icon-setting"
+          size="small"
+          @click="dialogVisible = true"
+          >自定义列</el-button
+        >
       </div>
     </div>
-    <el-table ref="table" :data="tempData" :header-cell-style="headerCellStyle" :cell-style="cellStyle" v-bind="$attrs"
-      v-on="$listeners">
+    <el-table
+      ref="table"
+      :data="tempData"
+      :header-cell-style="headerCellStyle"
+      :cell-style="cellStyle"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
       <template v-if="$slots.append" slot="append">
         <slot name="append"></slot>
       </template>
       <template v-for="item in copyColumns">
-        <el-table-column v-if="item.type && ['selection', 'index'].includes(item.type)" :key="`${item.prop}-if`"
-          v-bind="item">
+        <el-table-column
+          v-if="item.type && ['selection', 'index'].includes(item.type)"
+          :key="`${item.prop}-if`"
+          v-bind="item"
+        >
           <template #header="scope">
             <slot :name="`header-${item.prop}`" v-bind="scope"></slot>
           </template>
@@ -40,14 +54,29 @@
     </el-table>
     <div v-if="showPagination">
       <!-- 前端根据数据分页 -->
-      <el-pagination v-if="frontendPaging && !$scopedSlots[`paging`]" class="pagination" v-bind="tempPagination"
-        :current-page="currentPage" :page-size="fontPageSize" :total="fontTotal" @size-change="frontSizeChange"
-        @current-change="frontCurrentChange"></el-pagination>
+      <el-pagination
+        v-if="frontendPaging && !$scopedSlots[`paging`]"
+        class="pagination"
+        v-bind="tempPagination"
+        :current-page="currentPage"
+        :page-size="fontPageSize"
+        :total="fontTotal"
+        @size-change="frontSizeChange"
+        @current-change="frontCurrentChange"
+      ></el-pagination>
 
       <!-- 后端分页 -->
-      <el-pagination class="pagination" v-else-if="!frontendPaging && !$scopedSlots[`paging`]"
-        @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="endPage.currentPage"
-        :page-sizes="pageSizes" :page-size="endPage.pageSize" :layout="layout" :total="endPage.total">
+      <el-pagination
+        class="pagination"
+        v-else-if="!frontendPaging && !$scopedSlots[`paging`]"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="endPage.currentPage"
+        :page-sizes="pageSizes"
+        :page-size="endPage.pageSize"
+        :layout="layout"
+        :total="endPage.total"
+      >
       </el-pagination>
 
       <!-- 自定义分页 -->
@@ -61,13 +90,17 @@
       <template #title>
         <span class="dialog-title">自定义设置表格列</span>
       </template>
-      <div style="padding-bottom: 20px;">请选择在表格中显示的数据列</div>
+      <div style="padding-bottom: 20px">请选择在表格中显示的数据列</div>
       <el-card class="box-card" shadow="never">
         <div slot="header" class="clearfix">
           <span> <el-checkbox disabled>多选项</el-checkbox></span>
         </div>
         <el-checkbox-group v-model="checkList">
-          <el-checkbox v-for="(item, index) in columns" :key="index" :label="item.prop">
+          <el-checkbox
+            v-for="(item, index) in columns"
+            :key="index"
+            :label="item.prop"
+          >
             <el-tooltip placement="top" effect="light">
               <span slot="content">{{ item.label }}</span>
               <span class="isEllipsis">{{ item.label }}</span>
@@ -77,42 +110,47 @@
       </el-card>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false" size="small">取 消</el-button>
-        <el-button type="primary" @click="determine" size="small">确 定</el-button>
+        <el-button type="primary" @click="determine" size="small"
+          >确 定</el-button
+        >
       </span>
     </el-dialog>
   </div>
 </template>
 <script>
-const defaultPagination = { background: true, layout: 'total, sizes, prev, pager, next, jumper' }
+const defaultPagination = {
+  background: true,
+  layout: "total, sizes, prev, pager, next, jumper",
+};
 export default {
-  name: 'WangxiaoTable',
+  name: "WangxiaoTable",
   props: {
     columns: {
       type: Array,
-      default: () => []
+      default: () => [],
     },
     data: {
       type: Array,
-      default: () => []
+      default: () => [],
     },
     headerCellStyle: {
       type: Function || Object,
       default: () => {
         return {
-          'background-color': '#f5f6f7',
-          'font-size': '12px',
-          'padding': '10px',
-        }
-      }
+          "background-color": "#f5f6f7",
+          "font-size": "12px",
+          padding: "10px",
+        };
+      },
     },
     cellStyle: {
       type: Function || Object,
       default: () => {
         return {
-          'font-size': '12px',
-          'padding': '10px'
-        }
-      }
+          "font-size": "12px",
+          padding: "10px",
+        };
+      },
     },
     /** 是否显示分页 */
     showPagination: {
@@ -127,7 +165,7 @@ export default {
     /** 前端分页参数 */
     FrontPage: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
 
     /** 后端分页 */
@@ -142,29 +180,28 @@ export default {
         return {
           currentPage: 1,
           pageSize: 10,
-          total: 0
-        }
-      }
+          total: 0,
+        };
+      },
     },
     pageSizes: {
       default() {
         return [10, 20, 30, 50, 100];
       },
-      type: Array
+      type: Array,
     },
     layout: {
       default() {
-        return 'total, sizes, prev, pager, next, jumper';
+        return "total, sizes, prev, pager, next, jumper";
       },
-      type: String
+      type: String,
     },
 
     /** 是否显示自定义列 */
     showColumns: {
       default: false,
-      type: Boolean
+      type: Boolean,
     },
-
   },
   data() {
     return {
@@ -174,49 +211,56 @@ export default {
       dialogVisible: false,
       checkList: [],
       copyColumns: [],
-    }
+    };
   },
   computed: {
     paging() {
-      const offset = (this.currentPage - 1) * this.fontPageSize
-      return { offset, limit: this.fontPageSize }
+      const offset = (this.currentPage - 1) * this.fontPageSize;
+      return { offset, limit: this.fontPageSize };
     },
     fontTotal() {
-      return this.data?.length || 0
+      return this.data?.length || 0;
     },
     tempPagination() {
-      return { ...defaultPagination, ...this.FrontPage }
-    }
+      return { ...defaultPagination, ...this.FrontPage };
+    },
   },
   watch: {
-    'data': {
+    data: {
       handler() {
         this.tempData = this.data;
       },
       deep: true,
       immediate: true,
     },
+    columns: {
+      handler() {
+        this.copyColumns = this.columns;
+      },
+      deep: true,
+      immediate: true,
+    },
     FrontPage: {
       handler(nVal) {
-        this.currentPage = nVal.currentPage || 1
-        this.fontPageSize = nVal.fontPageSize || nVal.pageSizes?.[0] || 10
+        this.currentPage = nVal.currentPage || 1;
+        this.fontPageSize = nVal.fontPageSize || nVal.pageSizes?.[0] || 10;
       },
       immediate: true,
       deep: true,
     },
     paging: {
       handler() {
-        this.frontGetTableData()
+        this.frontGetTableData();
       },
       immediate: true,
       deep: true,
     },
   },
   mounted() {
-    const tempStore = this.$refs?.table || {}
+    const tempStore = this.$refs?.table || {};
     for (const key in tempStore) {
-      if (typeof tempStore[key] === 'function') {
-        this[key] = tempStore[key]
+      if (typeof tempStore[key] === "function") {
+        this[key] = tempStore[key];
       }
     }
     this.processingColum();
@@ -225,7 +269,7 @@ export default {
   methods: {
     /**
      * 前端分页
-     * @param {*} val 
+     * @param {*} val
      */
     frontSizeChange(val) {
       this.fontPageSize = val;
@@ -239,7 +283,9 @@ export default {
 
     frontGetTableData() {
       const { offset, limit } = this.paging || {};
-      this.tempData = this.data.filter((v, i) => i >= offset && i < (offset + limit))
+      this.tempData = this.data.filter(
+        (v, i) => i >= offset && i < offset + limit
+      );
     },
 
     /**
@@ -247,29 +293,30 @@ export default {
      * @param {*} val
      */
     handleCurrentChange(val) {
-      this.$emit("current-paging", val)
+      this.$emit("current-paging", val);
     },
 
     handleSizeChange(val) {
-      this.$emit("size-paging", val)
+      this.$emit("size-paging", val);
     },
 
     processingColum() {
       /** 列处理 */
       this.copyColumns = JSON.parse(JSON.stringify(this.columns));
-      let itemColum = this.columns.filter((item) => item.show !== false)
-      itemColum.map((item) => this.checkList.push(item.prop))
+      let itemColum = this.columns.filter((item) => item.show !== false);
+      itemColum.map((item) => this.checkList.push(item.prop));
     },
 
     determine() {
       this.dialogVisible = false;
-      let add = this.columns.filter(item => this.checkList.some(ele => ele === item.prop))
-      this.copyColumns = add
+      let add = this.columns.filter((item) =>
+        this.checkList.some((ele) => ele === item.prop)
+      );
+      this.copyColumns = add;
       this.$refs.table.doLayout();
-    }
-  }
-
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -285,32 +332,32 @@ export default {
   font-weight: bold;
 }
 
->>>.isEllipsis {
+>>> .isEllipsis {
   display: inline-block;
   width: 85px;
   overflow: hidden;
   text-overflow: ellipsis;
 }
 
->>>.el-checkbox {
+>>> .el-checkbox {
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  margin-right: 0px
+  margin-right: 0px;
 }
 
->>>.el-checkbox-group {
+>>> .el-checkbox-group {
   display: flex;
   flex-wrap: wrap;
   align-items: baseline;
   gap: 20px;
 }
 
->>>.el-dialog__body {
+>>> .el-dialog__body {
   padding: 10px 20px 10px 20px;
 }
 
->>>.el-card__header {
+>>> .el-card__header {
   padding: 10px 20px;
 }
 </style>
